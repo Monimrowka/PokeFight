61eb8e395c5aa5e492f4e99a5006feabea953ad5
Monimrowka <116653626+Monimrowka@users.noreply.github.com>
2023-01-30 17:06:15 +0100

    first steps2

--- backend/index.js
+++ backend/index.js
@@ -10,15 +10,15 @@ app.get('/pokemon', (req, res) => {
   console.log(data);
 })
 
-app.get('/pokemon/:id', (req, res) => {
-    const id = req.params
-    const allPokemons = data;
-    const targetPokemon = data.map((target) => {
-        return target.id === id
-    })
-    console.log(targetPokemon)
-    res.send('hello');
-})
+// app.get('/pokemon/:id', (req, res) => {
+//     const id = req.params
+//     const allPokemons = data;
+//     const targetPokemon = data.map((target) => {
+//         return target.id === id
+//     })
+//     console.log(targetPokemon)
+//     res.send('hello');
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
