332a0916568413e0aa0fe6608dd1bbfb5c662daa
Monimrowka <116653626+Monimrowka@users.noreply.github.com>
2023-01-30 16:59:59 +0100

    first steps

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const data = require('./data.json')
+
+app.get('/pokemon', (req, res) => {
+    // let pokemons = Object.keys(data)
+    // data.forEach
+  res.send(data)
+  console.log(data);
+})
+
+app.get('/pokemon/:id', (req, res) => {
+    const id = req.params
+    const allPokemons = data;
+    const targetPokemon = data.map((target) => {
+        return target.id === id
+    })
+    console.log(targetPokemon)
+    res.send('hello');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
