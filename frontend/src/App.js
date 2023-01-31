1fe46e0c4a4982bca367199c309cd13eb2e7b0e5
Agata Thrams <agata.thrams@gmail.com>
2023-01-31 15:45:26 +0100

    header and footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
+import Container from 'react-bootstrap/esm/Container';
 import './App.css';
+import PokeFooter from './components/generalComponents/PokeFooter';
+import PokeHeader from './components/generalComponents/PokeHeader';
 
 function App() {
   return (
     <div className="App">
+      <PokeHeader />
+      <Container className='appContainer'>
       Hello World from react app
+      </Container>
+      <PokeFooter />
     </div>
   );
 }
