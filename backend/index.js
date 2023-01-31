e4db132df73bb3c85d198924f53e8595f50e2172
Agata Thrams <agata.thrams@gmail.com>
2023-01-31 14:55:39 +0100

    managed the get id route

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,32 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3010
 const data = require('./data.json')
+var cors = require('cors')
+
+app.use(cors())
+app.use(express.json());
 
 app.get('/pokemon', (req, res) => {
-    // let pokemons = Object.keys(data)
-    // data.forEach
   res.send(data)
   console.log(data);
 })
 
-// app.get('/pokemon/:id', (req, res) => {
-//     const id = req.params
-//     const allPokemons = data;
-//     const targetPokemon = data.map((target) => {
-//         return target.id === id
-//     })
-//     console.log(targetPokemon)
-//     res.send('hello');
-// })
+app.get('/pokemon/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    const pokemon = data.find((value) => {
+      return value.id == id
+    })
+    res.status(200).json(
+      pokemon
+    );
+  } catch (error) {
+    res.status(500).json({
+      error,
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
