1889e835913920602886d61d048eecc84e022f14
Agata Thrams <agata.thrams@gmail.com>
2023-01-31 16:05:38 +0100

    axios, use effect and first state in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,32 @@ import PokeFooter from './components/generalComponents/PokeFooter';
 import PokeHeader from './components/generalComponents/PokeHeader';
 import AllPokemons from './components/routeComponents/AllPokemons';
 import PokemonById from './components/routeComponents/PokemonById';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3010/pokemons")
+      .then((response) => {
+        setPokemons(response.data);
+         console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <PokeHeader />
       <Container className='appContainer'>
         <h5>Hello World from react app</h5>
         <Routes>
-          <Route path="pokemons" element={<AllPokemons />} />
+          <Route path="pokemons" element={<AllPokemons pokemons={pokemons} />} />
           <Route path="pokemons/:id" element={<PokemonById />} />
         </Routes>
       </Container>
--- frontend/src/components/routeComponents/AllPokemons.js
+++ frontend/src/components/routeComponents/AllPokemons.js
@@ -1,6 +1,15 @@
 
-export default function AllPokemons() {
+export default function AllPokemons({pokemons}) {
   return (
-    <div>AllPokemons</div>
+    <div>
+           List of all pokemons
+            {/* {pokemons.map((pokemon) => {
+                return (
+                    <h3>{pokemon.name}</h3>
+                )
+            })} */}
+
+
+    </div>
   )
 }
